9e8dfefad9d19078e8ffee4c75f7a7c725fe9c88
Anisa <anishaghatan@gmail.com>
2026-08-17 19:38:40 +0545

    fixed css and added animation for car displayed

--- index.php
+++ index.php
@@ -148,7 +148,6 @@ error_reporting(0);
   </div>
   <!-- /Banners -->
 
-  <!-- Resent Cat-->
   <section class="section-padding gray-bg">
     <div class="container">
       <div class="section-header text-center">
@@ -166,53 +165,101 @@ error_reporting(0);
             <li role="presentation" class="active"><a href="#resentnewcar" role="tab" data-toggle="tab">New Car</a></li>
           </ul>
         </div>
-        <!-- Recently Listed New Cars -->
+
+        <!-- <section class="section-padding gray-bg">
+          <div class="container">
+            <div class="section-header text-center">
+              <h2>Find the Best <span>CarForYou</span></h2>
+              <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+                alteration in
+                some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            </div>
+            <div class="row">
+
+              <div class="recent-tab">
+                <ul class="nav nav-tabs" role="tablist">
+                  <li role="presentation" class="active"><a href="#resentnewcar" role="tab" data-toggle="tab">New
+                      Car</a></li>
+                </ul>
+              </div> -->
+
         <div class="tab-content">
           <div role="tabpanel" class="tab-pane active" id="resentnewcar">
+            <div class="car-grid">
 
-            <?php $sql = "SELECT tblvehicles.VehiclesTitle,tblbrands.BrandName,tblvehicles.PricePerDay,tblvehicles.FuelType,tblvehicles.ModelYear,tblvehicles.id,tblvehicles.SeatingCapacity,tblvehicles.VehiclesOverview,tblvehicles.Vimage1 from tblvehicles join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand limit 9";
-            $query = $dbh->prepare($sql);
-            $query->execute();
-            $results = $query->fetchAll(PDO::FETCH_OBJ);
-            $cnt = 1;
-            if ($query->rowCount() > 0) {
-              foreach ($results as $result) {
-                ?>
+              <?php
+              $sql = "SELECT tblvehicles.VehiclesTitle, tblbrands.BrandName, tblvehicles.PricePerDay,
+                             tblvehicles.FuelType, tblvehicles.ModelYear, tblvehicles.id,
+                             tblvehicles.SeatingCapacity, tblvehicles.VehiclesOverview, tblvehicles.Vimage1
+                      FROM tblvehicles
+                      JOIN tblbrands ON tblbrands.id = tblvehicles.VehiclesBrand
+                      LIMIT 9";
+              $query = $dbh->prepare($sql);
+              $query->execute();
+              $results = $query->fetchAll(PDO::FETCH_OBJ);
 
-                <div class="col-list-3">
-                  <div class="recent-car-list">
-                    <div class="car-info-box"> <a
-                        href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>"><img
-                          src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1); ?>" class="img-responsive"
-                          alt="image"></a>
-                      <ul>
-                        <li><i class="fa fa-car" aria-hidden="true"></i><?php echo htmlentities($result->FuelType); ?></li>
-                        <li><i class="fa fa-calendar" aria-hidden="true"></i><?php echo htmlentities($result->ModelYear); ?>
-                          Model</li>
-                        <li><i class="fa fa-user"
-                            aria-hidden="true"></i><?php echo htmlentities($result->SeatingCapacity); ?> seats</li>
-                      </ul>
-                    </div>
-                    <div class="car-title-m">
-                      <h6><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>">
-                          <?php echo htmlentities($result->VehiclesTitle); ?></a></h6>
-                      <span class="price">$<?php echo htmlentities($result->PricePerDay); ?> /Day</span>
-                    </div>
-                    <div class="inventory_info_m">
-                      <p><?php echo substr($result->VehiclesOverview, 0, 70); ?></p>
+              if ($query->rowCount() > 0) {
+                $i = 0;
+                foreach ($results as $result) {
+                  $delay = $i * 0.12;
+                  $overview = $result->VehiclesOverview
+                    ? substr(strip_tags($result->VehiclesOverview), 0, 70) . '...'
+                    : 'No description available.';
+                  ?>
+
+                  <div class="col-list-3 fade-up" style="animation-delay: <?php echo $delay; ?>s;">
+                    <div class="recent-car-list">
+
+                      <?php if ($i === 0) { ?>
+                        <span class="car-badge">New Car</span>
+                      <?php } ?>
+
+                      <div class="car-info-box">
+                        <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>" class="car-img-wrap">
+                          <img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1); ?>"
+                            alt="<?php echo htmlentities($result->VehiclesTitle); ?>">
+                        </a>
+                        <ul class="car-stats">
+                          <li><i class="fa fa-car" aria-hidden="true"></i><?php echo htmlentities($result->FuelType); ?>
+                          </li>
+                          <li><i class="fa fa-calendar"
+                              aria-hidden="true"></i><?php echo htmlentities($result->ModelYear); ?>
+                          </li>
+                          <li><i class="fa fa-user"
+                              aria-hidden="true"></i><?php echo htmlentities($result->SeatingCapacity); ?> seats</li>
+                        </ul>
+                      </div>
+
+                      <div class="car-body">
+                        <div class="car-title-m">
+                          <h6><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>">
+                              <?php echo htmlentities($result->VehiclesTitle); ?></a></h6>
+                          <span class="price">Rs.
+                            <?php echo number_format($result->PricePerDay); ?><small>/Day</small></span>
+                        </div>
+                        <div class="inventory_info_m">
+                          <p><?php echo htmlentities($overview); ?></p>
+                        </div>
+                      </div>
+
                     </div>
                   </div>
-                </div>
-              <?php }
-            } ?>
 
+                  <?php
+                  $i++;
+                }
+              } else {
+                echo '<p class="text-center">No vehicles found.</p>';
+              }
+              ?>
+
+            </div>
           </div>
         </div>
       </div>
+    </div>
   </section>
-  <!-- /Resent Cat -->
-
-  <!-- Fun Facts-->
+  <!-- recently listed cars  -->
   <section class="fun-facts-section">
     <div class="container div_zindex">
       <div class="row">
@@ -304,7 +351,8 @@ error_reporting(0);
   <!-- /Footer-->
 
   <!--Back to top-->
-  <div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+  <div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a>
+  </div>
   <!--/Back to top-->
 
   <!--Login-Form -->
